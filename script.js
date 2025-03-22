35869ed0ce8134587ba76a46da723a6b1c4e6909
MrSlushious <testerista9@gmail.com>
2025-03-22 01:02:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("downloadBtn").addEventListener("click", function() {
+    // Change this link to your actual download link
+    let downloadLink = "https://www.mediafire.com/file/qj17y1t3ud686ge/PPOR.zip/file";
+    window.location.href = downloadLink;
+});
